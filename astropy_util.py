b74326c63cf92e7ecef574e2dd9fa6c94981b462
Gregory Simonian <simonian@astronomy.ohio-state.edu>
2018-01-25 21:08:21 -0500

    Added consume to the itertools recipe box.

--- astropy_util.py
+++ astropy_util.py
@@ -478,3 +478,13 @@ def powerset(iterable):
     "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(len(s)+1))
+
+def consume(iterator, n):
+    "Advance the iterator n-steps ahead. If n is none, consume entirely."
+    # Use functions that consume iterators at C speed.
+    if n is None:
+        # feed the entire iterator into a zero-length deque
+        collections.deque(iterator, maxlen=0)
+    else:
+        # advance to the empty slice starting at position n
+        next(islice(iterator, n, n), None)
